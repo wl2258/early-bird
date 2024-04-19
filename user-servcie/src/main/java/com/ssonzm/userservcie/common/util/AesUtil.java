49a6ba6d4eb43b6c4d5b404f73521911782a16ac
jimin <wl2258@naver.com>
2024-04-19 10:14:28 +0900

    feature: Users 엔티티 db에 저장 시 개인 정보 AES 알고리즘 적용해 암호화
    - 유니크 암호화, 복호화 적용

---
+++ user-servcie/src/main/java/com/ssonzm/userservcie/common/util/AesUtil.java
@@ -0,0 +1,82 @@
+package com.ssonzm.userservcie.common.util;
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+@Component
+public class AesUtil {
+    private static final int IV_LENGTH = 16;
+
+    private String secretKey; // 32byte
+
+    private String iv = "";
+
+    private static final String AES_METHOD = "AES/CBC/PKCS5Padding";
+
+    private static final String CIPHER_FINAL_ENCODING = "UTF-8";
+
+    private static final String ENCODING_METHOD = "AES";
+
+    private final Environment env;
+    public AesUtil(Environment env) {
+        this.env = env;
+        secretKey = env.getProperty("aes.secret");
+    }
+
+    /**
+     * RuntimeException으로 Wrapping헀으므로 RuntimeException을 캐치할 것.
+     * random으로 하는 경우 unique 체크가 어려우므로 salt를 같은 값으로 처리함
+     * @param targetEncodeString
+     * @return
+     */
+    public String encodeUnique(String targetEncodeString) {
+        try {
+            iv = secretKey.substring(0, IV_LENGTH);
+
+            byte[] keyData = secretKey.getBytes();
+
+            SecretKey secureKey = new SecretKeySpec(keyData, ENCODING_METHOD);
+
+            Cipher c = Cipher.getInstance(AES_METHOD);
+            c.init(Cipher.ENCRYPT_MODE, secureKey, new IvParameterSpec(iv.getBytes()));
+
+            byte[] encrypted = c.doFinal(targetEncodeString.getBytes(CIPHER_FINAL_ENCODING));
+            String enStr = new String(Base64.getEncoder().encode(encrypted));
+
+            return enStr;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * RuntimeException으로 Wrapping헀으므로 RuntimeException을 캐치할 것.
+     * @param targetDecodeString
+     * @return
+     */
+    public String decodeUnique(String targetDecodeString) {
+        try {
+            iv = secretKey.substring(0, IV_LENGTH);
+
+            byte[] keyData = secretKey.getBytes();
+            SecretKey secureKey = new SecretKeySpec(keyData, ENCODING_METHOD);
+            Cipher c = Cipher.getInstance(AES_METHOD);
+            c.init(Cipher.DECRYPT_MODE, secureKey, new IvParameterSpec(iv.getBytes(CIPHER_FINAL_ENCODING)));
+
+            byte[] byteStr = Base64.getDecoder().decode(targetDecodeString.getBytes());
+
+            return new String(c.doFinal(byteStr), CIPHER_FINAL_ENCODING);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
