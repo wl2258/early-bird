ad360237ca18fda3c41cfa2c93f12cd77432e19c
jimin <wl2258@naver.com>
2024-04-20 18:08:01 +0900

    feature: 상품 조회

--- user-servcie/src/main/java/com/ssonzm/userservcie/service/product/ProductService.java
+++ user-servcie/src/main/java/com/ssonzm/userservcie/service/product/ProductService.java
@@ -1,7 +1,13 @@
 package com.ssonzm.userservcie.service.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import static com.ssonzm.userservcie.dto.product.ProductRequestDto.*;
+import static com.ssonzm.userservcie.vo.product.ProductResponseVo.*;
 
 public interface ProductService {
     Long saveProduct(Long userId, ProductSaveReqDto productSaveReqDto);
+
+    Page<ProductListRespVo> getProductList(Pageable pageable);
 }
