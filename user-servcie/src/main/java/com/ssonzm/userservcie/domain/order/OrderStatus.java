7b97faaadcef60211ee93854dd617a351d2fde2b
jimin <wl2258@naver.com>
2024-04-22 01:11:35 +0900

    feature: 결제 도메인 생성

--- user-servcie/src/main/java/com/ssonzm/userservcie/domain/order/OrderStatus.java
+++ user-servcie/src/main/java/com/ssonzm/userservcie/domain/order/OrderStatus.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum OrderStatus {
 
-    CREATED("신규주문"), CANCELED("주문취소");
+    CREATED("신규 주문"), CANCELED("주문 취소");
     private String value;
 
 }
