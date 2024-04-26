c8773854b3479643f1654aaa26d0a296d07eacc2
jimin <wl2258@naver.com>
2024-04-26 15:43:21 +0900

    Init order service - eureka client setting

--- early-bird-msa/order-service/src/main/java/com/ssonzm/orderservice/OrderServiceApplication.java
+++ early-bird-msa/order-service/src/main/java/com/ssonzm/orderservice/OrderServiceApplication.java
@@ -2,12 +2,14 @@ package com.ssonzm.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
