5cf561a825da0f47b9dc524a9a86887a517144ce
jimin <wl2258@naver.com>
2024-04-27 10:24:49 +0900

    update: change micro service ip address

--- early-bird-msa/product-service/src/main/java/com/ssonzm/productservice/config/SecurityConfig.java
+++ early-bird-msa/product-service/src/main/java/com/ssonzm/productservice/config/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.ssonzm.productservice.config;
 
-import com.ssonzm.coremodule.util.ResponseUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,7 +33,7 @@ public class SecurityConfig {
                                 .requestMatchers("/**").access(
                                         new WebExpressionAuthorizationManager(
                                                 "hasIpAddress('127.0.0.1') or hasIpAddress('::1')" +
-                                                        "or hasIpAddress('172.30.1.37')"
+                                                        "or hasIpAddress('172.30.1.60')"
                                         )
                                 )
                                 .anyRequest().authenticated()
