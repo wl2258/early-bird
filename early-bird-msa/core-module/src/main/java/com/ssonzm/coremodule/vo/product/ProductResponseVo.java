665632f03b102d5737008f577d18577fa1434f16
jimin <wl2258@naver.com>
2024-05-01 17:31:41 +0900

    feature: 상품 조회 시 reservationStartTime 필드 추가

--- early-bird-msa/core-module/src/main/java/com/ssonzm/coremodule/vo/product/ProductResponseVo.java
+++ early-bird-msa/core-module/src/main/java/com/ssonzm/coremodule/vo/product/ProductResponseVo.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 public class ProductResponseVo {
 
     @Data
@@ -18,5 +20,6 @@ public class ProductResponseVo {
         private int quantity;
         private int price;
         private String description;
+        private LocalDateTime reservationStartTime;
     }
 }
