1704a39e244ec7770efdc07b78f8b109cfe19ffe
jimin <wl2258@naver.com>
2024-05-06 20:28:25 +0900

    update: 카프카 리스너 토픽 변경

--- early-bird-msa/core-module/src/main/java/com/ssonzm/coremodule/vo/KafkaVo.java
+++ early-bird-msa/core-module/src/main/java/com/ssonzm/coremodule/vo/KafkaVo.java
@@ -2,6 +2,6 @@ package com.ssonzm.coremodule.vo;
 
 public interface KafkaVo {
     String KAFKA_PRODUCT_TOPIC = "early-bird-product";
-    String KAFKA_PRODUCT_ROLLBACK_TOPIC = "early-bird-rollback-product";
+    String KAFKA_PRODUCT_ROLLBACK_TOPIC = "early-bird-product-rollback";
 
 }
